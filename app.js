83cc31880323cba255977ddf88c8e589ef86132e
Gustavo <gggouvea@hotmail.com>
2020-03-27 18:10:26 -0300

    iterations 1, 2 & 3 done

--- app.js
+++ app.js
@@ -29,6 +29,54 @@ app.get('/', function (req, res) {
   })
 
 
+app.get('/artist-search', function (req, res) {
+    spotifyApi
+        .searchArtists(req.query.artist)
+        .then(data => {
+        console.log('Search artists by "artist"', data.body);
+        // res.send(data)
+        let artist = data.body.artists.items;
+        res.render('artist-search-results', {
+            artist
+        })
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+    })
+
+    app.get('/albums/:artistId', (req, res, next) => {
+        spotifyApi
+            .getArtistAlbums(req.query.artistId)
+            .then(function(data) {
+                console.log('Artist albums', data.body);
+            }, function(err) {
+                console.error(err);
+            });
+      });
+
+app.get('/albums/:artistId', function (req, res) {
+    spotifyApi
+        .getArtistAlbums(req.query.artistId)
+        .then(data => {
+        console.log('Artist albums', data.body);
+        // res.send(data)
+        let artist = data.body.artists.items;
+        
+        res.render('artist-search-results', {
+            artist
+        })
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+    })
+
+
+
+   
+
+
+
+
+
+
 
 
 // run server
